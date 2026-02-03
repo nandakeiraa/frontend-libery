c05fb30a30b39eaf4c66a05e8b0cd2a5e1a5522d
Nanda Keira <skm1223455@gmail.com>
2026-02-02 19:28:07 -0800

    feat:memperbaki siswadashboard

--- src/pages/SiswaDashboard.jsx
+++ src/pages/SiswaDashboard.jsx
@@ -1,10 +1,38 @@
+import Navbar from "../components/Navbar";
+
 export default function SiswaDashboard() {
   const user = JSON.parse(localStorage.getItem("user"));
 
   return (
-    <div>
-      <h1>Dashboard Siswa</h1>
-      <p>Halo, {user.name}</p>
-    </div>
+    <>
+      <Navbar title="Dashboard Siswa" />
+
+      <div style={styles.container}>
+        <h1>Dashboard Siswa</h1>
+
+        <p>
+          Halo, <strong>{user?.name}</strong>
+        </p>
+
+        <div style={styles.info}>
+          <p>Role: {user?.role}</p>
+        </div>
+      </div>
+    </>
   );
 }
+
+const styles = {
+  container: {
+    padding: "30px",
+    minHeight: "100vh",
+    background: "#f1f5f9",
+  },
+  info: {
+    marginTop: "20px",
+    background: "white",
+    padding: "15px",
+    borderRadius: "8px",
+    width: "250px",
+  },
+};
